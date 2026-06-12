4631a60b468df1230ea8b15efa357e181debb57a
Marco Gambino <marcogambino2@gmail.com>
2026-06-12 14:20:45 +0200

    Remove analysis and plotting sections from main.m

--- main.m
+++ main.m
@@ -19,16 +19,3 @@ canale_parietale_nome = 'P3';
 
 canale_frontale_idx = find(strcmp(canali_nomi, canale_frontale_nome));
 canale_parietale_idx = find(strcmp(canali_nomi, canale_parietale_nome));
-
-%% Model Order Selection
-
-order_selection(EEG, canale_frontale_idx, canale_parietale_idx, pfilter, pmax, p);
-
-%% Statistical Analysis
-
-[var_oa, var_oc, gc_front_par_oa, gc_front_par_oc, gc_par_front_oa, gc_par_front_oc] = ...
-    statistical_analysis(EEG, canale_frontale_idx, canale_parietale_idx, pfilter, p);
-
-%% Plots and Statistical Tests
-
-plots(var_oa, var_oc, gc_front_par_oa, gc_front_par_oc, gc_par_front_oa, gc_par_front_oc);
